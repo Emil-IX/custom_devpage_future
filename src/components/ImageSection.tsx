bd7c54b483da1dd3ff1cd49e45f34065fabf34d2
Emil-Spi <xiriusix@gmail.com>
2025-11-18 16:57:39 -0400

    fix: animations

--- src/components/ImageSection.tsx
+++ src/components/ImageSection.tsx
@@ -4,14 +4,14 @@ const ImageSection = () => {
 
     return (
         <div className="ImageSection">
-            <div className="ImageSection_texts appearleft">
-                <h3>Software services</h3>
-                <ul>
-                    <li>Specialists in web and application development</li>
-                    <li>Creation with different architectures such as client-server, monolithic, MVC, events, etc.</li>
-                    <li>Maintenance and support with servers and version control</li>
-                    <li>Quality assurance and quality control</li>
-                    <li>Deployment and hosting</li>
+            <div className="ImageSection_texts">
+                <h3 className="appear">Software services</h3>
+                <ul className="list">
+                    <li className="appearleft">Specialists in web and application development</li>
+                    <li className="appearleft">Creation with different architectures such as client-server, monolithic, MVC, events, etc.</li>
+                    <li className="appearleft">Maintenance and support with servers and version control</li>
+                    <li className="appearleft">Quality assurance and quality control</li>
+                    <li className="appearleft">Deployment and hosting</li>
                 </ul>
             </div>
         </div>
