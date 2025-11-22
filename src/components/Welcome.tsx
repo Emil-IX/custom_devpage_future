2cabe1135d6489cf158538bb8c76d4d53e99f4c6
Emil-Spi <xiriusix@gmail.com>
2025-11-22 13:00:59 -0400

    fix: text decription

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -8,10 +8,10 @@ const Welcome = () => {
             <div className="fadeout">
                 <h3 className="welcome_title appear">Hello there!</h3>
                 <p className="welcome_text">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    In today, s digital world, software development is essential for solving real problems and driving innovation across every industry. Businesses of all sizes rely on modern applications to automate tasks, improve decision-making, reduce costs, and deliver faster, more intuitive experiences. Software is not only about writing code—it is about creating solutions that help people and organizations work smarter and stay competitive in a constantly evolving market.
                 </p>
                 <p className="welcome_text">
-                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" The Extremes of Good and Evil by Cicero, written in 45 BC.
+                    With growing expectations for reliable and secure technology, Quality Assurance plays a crucial role in ensuring that software is consistent, high-performing, and safe for users. Together, development and quality assurance build solutions that are scalable, trustworthy, and impactful. This powerful combination shapes the way we interact with the digital world and creates technology that truly makes a difference.
                 </p>
             </div>
 
