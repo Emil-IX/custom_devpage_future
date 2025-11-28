ee1d5ddb1294978ead0c9a603d3d1234fc6cb671
Emil-Spi <xiriusix@gmail.com>
2025-11-28 00:31:20 -0400

    feat: click copy on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,25 @@
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Footer = () => {
+
+const navigate = useNavigate()
+
+  const [copied, setCopied] = useState(false);
+
+
+  const handleCopy = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      setCopied(true);
+
+      setTimeout(() => setCopied(false), 2000);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  
     return (
         <footer>
             <div id="footer" className="footerContent">
@@ -15,9 +34,15 @@ export const Footer = () => {
                     <a href="#about">About</a>
                     <a href="#proyects">Proyects</a>
                 </div>
+                <div className="medias">
                 <div className="icons">
-                    <div className="image"><img src="/linkedin.svg" alt="" /></div>
-                    <div className="image"><img src="/github2.svg" alt="" /></div>
+                    <div className="image" onClick={() => navigate('https://www.linkedin.com/in/emil-h-espiritu-245732248')} ><img className="ink" src="/linkedinwhite.webp" alt="" /></div>
+                    <div className="image" onClick={() => navigate('https://github.com/Emil-IX')}><img className="git" src="/github2.png" alt="" /></div>
+                </div>
+                    <div className="email">
+                        <input type="text" value='emilspiritu@gmail.com'/>
+                        <button onClick={() => handleCopy("emilspiritu@gmail.com")}> {copied ? "Copied" : "Copy"}</button>
+                    </div>
                 </div>
             </div>
         </footer>
