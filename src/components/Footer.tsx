94e6244c127c64db0584d1264eaef1ea3ce564e5
Emil-Spi <xiriusix@gmail.com>
2025-11-28 10:53:31 -0400

    feat: responsive design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,23 +3,23 @@ import { useNavigate } from "react-router-dom";
 
 export const Footer = () => {
 
-const navigate = useNavigate()
+    const navigate = useNavigate()
 
-  const [copied, setCopied] = useState(false);
+    const [copied, setCopied] = useState(false);
 
 
-  const handleCopy = async (text: string) => {
-    try {
-      await navigator.clipboard.writeText(text);
-      setCopied(true);
+    const handleCopy = async (text: string) => {
+        try {
+            await navigator.clipboard.writeText(text);
+            setCopied(true);
 
-      setTimeout(() => setCopied(false), 2000);
-    } catch (error) {
-      console.error(error);
+            setTimeout(() => setCopied(false), 2000);
+        } catch (error) {
+            console.error(error);
+        }
     }
-  }
 
-  
+
     return (
         <footer>
             <div id="footer" className="footerContent">
@@ -35,12 +35,12 @@ const navigate = useNavigate()
                     <a href="#proyects">Proyects</a>
                 </div>
                 <div className="medias">
-                <div className="icons">
-                    <div className="image" onClick={() => navigate('https://www.linkedin.com/in/emil-h-espiritu-245732248')} ><img className="ink" src="/linkedinwhite.webp" alt="" /></div>
-                    <div className="image" onClick={() => navigate('https://github.com/Emil-IX')}><img className="git" src="/github2.png" alt="" /></div>
-                </div>
+                    <div className="icons">
+                        <div className="image" onClick={() => navigate('https://www.linkedin.com/in/emil-h-espiritu-245732248')} ><img className="ink" src="/linkedinwhite.webp" alt="" /></div>
+                        <div className="image" onClick={() => navigate('https://github.com/Emil-IX')}><img className="git" src="/github2.png" alt="" /></div>
+                    </div>
                     <div className="email">
-                        <input type="text" value='emilspiritu@gmail.com'/>
+                        <input type="text" value='emilspiritu@gmail.com' />
                         <button onClick={() => handleCopy("emilspiritu@gmail.com")}> {copied ? "Copied" : "Copy"}</button>
                     </div>
                 </div>
