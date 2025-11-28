0529ed0e5e26aa92bc082c673e4ef761df1962e3
Emil-Spi <xiriusix@gmail.com>
2025-11-28 09:07:38 -0400

    fix: animation appear

--- src/components/Proyects.tsx
+++ src/components/Proyects.tsx
@@ -1,13 +1,17 @@
+import { useNavigate } from "react-router-dom"
 
 
 export const Proyects = () => {
+
+    const navigate = useNavigate()
+
     return (
         <div id="proyects" className="proyects_container">
 
             <div className="tech_title--container appear">
                 <h2 className="tech_title tech_title--color">Projects</h2>
                 <p className="color-white">
-                  Here are some of my projects that use the technologies used, along with their GitHub links for more details.
+                    Here are some of my projects that use the technologies used, along with their GitHub links for more details.
                 </p>
             </div>
 
@@ -28,6 +32,7 @@ export const Proyects = () => {
                         <p>MongoDB</p>
                     </div>
                 </div>
+
                 <div className="pro_element appearright">
                     <h3 className="pro_element_title">ITE-Commerce</h3>
                     <p>
@@ -35,7 +40,7 @@ export const Proyects = () => {
                     </p>
                     <a href="https://github.com/Emil-IX/ITE-commerce-app">View in github</a>
                     <div className="pro_tags">
-                        <p>Javascripts</p>
+                        <p>Javascript</p>
                         <p>React</p>
                         <p>Node.js</p>
                         <p>Express</p>
@@ -43,6 +48,35 @@ export const Proyects = () => {
                     </div>
                 </div>
 
+                <div className="pro_element appearleft2">
+                    <h3 className="pro_element_title">Mentor clock</h3>
+                    <p>
+                        An Frontend mentor challenge with its own additions to make it even more interesting.An application that shows different ways to display the time, stopwatches, clocks, timers, and a weather add-on.
+                    </p>
+                    <a href="https://github.com/Emil-IX/Mentor-Clock">View in github</a>
+                    <div className="pro_tags">
+                        <p>HTML</p>
+                        <p>CSS</p>
+                        <p>Javascripts</p>
+                        <p>React</p>
+                    </div>
+                </div>
+
+                <div className="pro_element appearright">
+                    <h3 className="pro_element_title">Shiny-dex</h3>
+                    <p>
+                        A dynamic web application that leverages the PokeAPI to fetch, display, and manage Pokémon data in an appealing card format. Catch a glimpse of your favorite Pokémon, filter the collection, try your luck at finding Shinies.
+                    </p>
+                    <div className="gitLinks">
+                        <a href="https://github.com/Emil-IX/Pokemon_Api_shynidex">View in github</a>
+                        <img src="./links.png" alt="link" onClick={()=> navigate('https://emil-ix.github.io/Pokemon_Api_shynidex/')} />
+                    </div>
+                    <div className="pro_tags">
+                        <p>HTML</p>
+                        <p>CSS</p>
+                        <p>Javascripts</p>
+                    </div>
+                </div>
 
             </div>
         </div>
